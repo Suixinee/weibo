39434336524b26a65c2c22016a8142df435f7d3b
vagrant <vagrant@homestead>
2021-06-19 08:53:57 +0000

    发送找回密码

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,12 @@ Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 //激活
 Route::get('/signup/confirm/{token}','UsersController@confirmEmail')->name('confirm_email');
+//修改密码
+Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset',  'PasswordController@reset')->name('password.update');
+
 Route::resource('users','UsersController');
 Route::fallback(function(){
     echo '404';
